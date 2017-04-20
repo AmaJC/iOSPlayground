666eac20a358039e8b69215d83294acece50ba78
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2017-04-19 20:38:08 -0700

    Adding User class

--- MapkitDemo/MapkitDemo/ViewController.swift
+++ MapkitDemo/MapkitDemo/ViewController.swift
@@ -12,17 +12,31 @@ import MapKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let initialLocation = CLLocation(latitude: 37.8719, longitude: -122.2585)
+        centerMapOnLocation(location: initialLocation)
+        self.mapView.showsUserLocation = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
-
+    
+    func makeAnnotation(location: CLLocation) {
+        
+        
+    }
+    
+    func updateAnnotation(location: CLLocation) {
+        
+        
+    }
 
 }
 
